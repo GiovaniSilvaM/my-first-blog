30035a79d16d6632b81c207836880491c8f1543d
giovanni <gi.silvam@alumnos.duoc.cl>
2019-12-05 18:34:59 -0300

    Html para la pagina

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=200)
-    correo = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -16,4 +16,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.nombre
+        return self.title
