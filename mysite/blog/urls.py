d43716eea87417cb29d173c672bfcf51a3510531
giovanni <gi.silvam@alumnos.duoc.cl>
2019-12-06 03:54:54 -0300

    Parte en inicio de sesion

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from blog.views import post_detail
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('',views.post_detail,name='post_detail'),
+    
 ]
